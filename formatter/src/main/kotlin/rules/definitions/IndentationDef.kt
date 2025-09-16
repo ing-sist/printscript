36117c76ec50a20c92eb52e4cf5c14d172bb5420
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-09-15 23:15:46 -0300

    changed true to false default fixed

--- formatter/src/main/kotlin/rules/definitions/IndentationDef.kt
+++ formatter/src/main/kotlin/rules/definitions/IndentationDef.kt
@@ -4,7 +4,7 @@ import config.RuleOwner
 
 object IndentationDef : Rule<Int> {
     override val id: String = "indentation"
-    override val default: Int = 4
+    override val default: Int = 2
     override val owner: RuleOwner = RuleOwner.USER
 
     override fun parse(raw: String): Int = raw.trim().toInt()
