e43457d9eab0f7430d0cf5a6df9f0c60581449f5
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-11-11 14:19:07 -0300

    deleted comments

--- formatter/src/main/kotlin/config/DefaultRuleAdapter.kt
+++ formatter/src/main/kotlin/config/DefaultRuleAdapter.kt
@@ -16,13 +16,12 @@ object DefaultRuleAdapter : RuleIdNameAdapter {
             "indent-inside-if" to { _ -> RuleMapping(IndentationDef) { it } },
             "mandatory-line-break-after-statement" to { _ -> RuleMapping(LineBreakAfterSemiColonDef) { it } },
             "mandatory-single-space-separation" to { _ -> RuleMapping(MaxSpaceBetweenTokensDef) { it } },
-            // 2: casos especiales
             "enforce-no-spacing-around-equals" to { _ ->
                 RuleMapping(SpaceAroundAssignmentDef) { v ->
                     when (v) {
                         null -> null
                         is Boolean -> !v
-                        else -> { /* ignore / default */ }
+                        else -> {}
                     }
                 }
             },
@@ -30,31 +29,3 @@ object DefaultRuleAdapter : RuleIdNameAdapter {
 
     override fun resolve(name: String): RuleMapping? = mappings[name]?.invoke(null)
 }
-
-// object DefaultRuleAdapter : RuleIdNameAdapter {
-//    override fun resolve(name: String): RuleMapping? =
-//        when (name) {
-//            "SpaceAroundOperators" -> RuleMapping(SpaceAroundOperatorsDef) { it }
-//            "enforce-spacing-around-equals" -> RuleMapping(SpaceAroundAssignmentDef) { it }
-//            "enforce-no-spacing-around-equals" ->
-//                RuleMapping(SpaceAroundAssignmentDef) { v ->
-//                    when (v) {
-//                        null -> null
-//                        is Boolean -> !v
-//                        else -> {}
-//                    }
-//                }
-//            "Indentation" -> RuleMapping(IndentationDef) { it }
-//            "KeywordSpacingAfter" -> RuleMapping(KeywordSpacingAfterDef) { it }
-//            "line-breaks-after-println" -> RuleMapping(LineBreakBeforePrintlnDef) { it }
-//            "mandatory-space-surrounding-operations" -> RuleMapping(SpaceAroundOperatorsDef) { it }
-//            "enforce-spacing-before-colon-in-declaration" -> RuleMapping(SpaceBeforeColonDef) { it }
-//            "enforce-spacing-after-colon-in-declaration" -> RuleMapping(SpaceAfterColonDef) { it }
-//            "if-brace-below-line" -> RuleMapping(BelowLineBraceIfStatementDef) { it }
-//            "if-brace-same-line" -> RuleMapping(InlineBraceIfStatementIdDef) { it }
-//            "indent-inside-if" -> RuleMapping(IndentationDef) { it }
-//            "mandatory-line-break-after-statement" -> RuleMapping(LineBreakAfterSemiColonDef) { it }
-//            "mandatory-single-space-separation" -> RuleMapping(MaxSpaceBetweenTokensDef) { it }
-//            else -> null
-//        }
-// }
